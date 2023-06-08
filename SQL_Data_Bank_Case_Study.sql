b3436eaf7e2bed8605a0d5606720f9fdcfae79c3
Sakshi <sakshi0317@gmail.com>
2023-06-09 03:02:19 +0530

    Second Commit

---
+++ SQL_Data_Bank_Case_Study.sql
@@ -0,0 +1,13 @@
+---------- Data Bank Case Study ----------
+
+-- Database
+use Data_Bank
+
+--Tables
+select * from customer_nodes
+select * from customer_transactions
+select * from regions
+
+
+---------- Case Study Questions ----------
+
