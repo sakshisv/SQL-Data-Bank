3213df107d3ebe459be4a53f748cec65de33e859
Sakshi <sakshi0317sv@gmail.com>
2023-06-23 02:43:34 +0530

    Twenty-ninth Commit

--- SQL_Data_Bank_Case_Study.sql
+++ SQL_Data_Bank_Case_Study.sql
@@ -72,13 +72,39 @@ select avg(deposit_count) avg_deposit, avg(total_amount) avg_amount from total_c
 
 --Q3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
 
+with txn_type_cte as (
 select customer_id, DATENAME(MONTH, txn_date) month_name, month(txn_date) months,
 sum(case when txn_type = 'deposit' then 1 else 0 end) as deposit_count,
 sum(case when txn_type = 'purchase' then 1 else 0 end) as purchase_count,
 sum(case when txn_type = 'withdrawl' then 1 else 0 end) as withdrawl_count
 from customer_transactions
-group by customer_id, DATENAME(MONTH, txn_date), month(txn_date)
+group by customer_id, DATENAME(MONTH, txn_date), month(txn_date))
 
+select month_name, months, count(customer_id) cust_count 
+from txn_type_cte
+where deposit_count > 1 and (purchase_count >= 1 or withdrawl_count >= 1)
+group by month_name, months
+
+
+WITH monthly_transactions AS (
+  SELECT 
+    customer_id, 
+    month(txn_date) AS mth,
+    SUM(CASE WHEN txn_type = 'deposit' THEN 0 ELSE 1 END) AS deposit_count,
+    SUM(CASE WHEN txn_type = 'purchase' THEN 0 ELSE 1 END) AS purchase_count,
+    SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS withdrawal_count
+  FROM customer_transactions
+  GROUP BY customer_id, month(txn_date)
+)
+
+SELECT
+  mth,
+  COUNT(DISTINCT customer_id) AS customer_count
+FROM monthly_transactions
+WHERE deposit_count > 1 
+  AND (purchase_count >= 1 OR withdrawal_count >= 1)
+GROUP BY mth
+ORDER BY mth;
 
 
 
